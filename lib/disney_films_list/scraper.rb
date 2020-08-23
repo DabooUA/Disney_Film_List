f27b2620c96acfbc3e2aa7f0588f7b7d422ddb82
DabooUA <sbardysh@tutanota.com>
2020-08-23 12:53:57 -0500

    refactoring get_films method to add NR (Not Reated) to Films that do not have rating listed

--- lib/disney_films_list/scraper.rb
+++ lib/disney_films_list/scraper.rb
@@ -5,13 +5,15 @@ require 'pry'
 class DisneyFilmsList::Scraper
 
     def self.get_films
-        doc = Nokogiri::HTML(open(https://d23.com/list-of-disney-films/))
-        doc.css(".entry-content").each.with_index(1) do |row|
-        puts row.css('p').text.strip.split("\n")
+        site = "https://d23.com/list-of-disney-films/"
+        doc = Nokogiri::HTML(open(site))
+        doc.css(".entry-content").each do |row| 
+        puts row.css('p').text.strip.gsub(/\b$\b/, ' (NR)').split("\n") [1..750]
+               
         binding.pry 
-
+       
         end
-
+      
     end
 
 end
